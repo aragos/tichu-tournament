71ea291a9a8d406b79b2585252b628cf194630d2
Anna <teytanna@gmail.com>
2016-12-17 20:07:22 -0500

    Add delete test to tournament_handler_test

--- api/test/tournament_handler_test.py
+++ api/test/tournament_handler_test.py
@@ -77,14 +77,16 @@ class AppTest(unittest.TestCase):
     id = self.AddBasicTournament()
     self.logoutUser()
     params = {'name': 'Name', 'no_pairs': 8, 'no_boards': 24}
-    response = self.testapp.put_json("/api/tournaments/{}".format(id), params, expect_errors=True)
+    response = self.testapp.put_json("/api/tournaments/{}".format(id), params,
+                                     expect_errors=True)
     self.assertEqual(response.status_int, 401)
   
   def testPutTournament_bad_id(self):
     self.loginUser()
     id = self.AddBasicTournament()
     params = {'name': 'name', 'no_pairs': 8, 'no_boards': 24}
-    response = self.testapp.put_json("/api/tournaments/{}a".format(id), params, expect_errors=True)
+    response = self.testapp.put_json("/api/tournaments/{}a".format(id), params,
+                                     expect_errors=True)
     self.assertEqual(response.status_int, 404)
 
   def testPutTournament_not_owner(self):
@@ -92,51 +94,60 @@ class AppTest(unittest.TestCase):
     id = self.AddBasicTournament()
     self.loginUser('user2@example.com', id='234')
     params = {'name': 'name', 'no_pairs': 8, 'no_boards': 24}
-    response = self.testapp.put_json("/api/tournaments/{}".format(id), params, expect_errors=True)
+    response = self.testapp.put_json("/api/tournaments/{}".format(id), params,
+                                     expect_errors=True)
     self.assertEqual(response.status_int, 403)
     
   def testPutTournament_empty_name(self):
     self.loginUser()
     id = self.AddBasicTournament()
     params = {'name': '', 'no_pairs': 8, 'no_boards': 24}
-    response = self.testapp.put_json("/api/tournaments/{}".format(id), params, expect_errors=True)
+    response = self.testapp.put_json("/api/tournaments/{}".format(id), params,
+                                     expect_errors=True)
     self.assertEqual(response.status_int, 400)
     
   def testPutTournament_bad_num_pairs(self):
     self.loginUser()
     id = self.AddBasicTournament()
     params = {'name': 'name1', 'no_pairs': '8a', 'no_boards': 24}
-    response = self.testapp.put_json("/api/tournaments/{}".format(id), params, expect_errors=True)
+    response = self.testapp.put_json("/api/tournaments/{}".format(id), params,
+                                     expect_errors=True)
     self.assertEqual(response.status_int, 400)
     
   def testPutTournament_bad_num_boards(self):
     self.loginUser()
     id = self.AddBasicTournament()
     params = {'name': 'name1', 'no_pairs': 8, 'no_boards': '24b'}
-    response = self.testapp.put_json("/api/tournaments/{}".format(id), params, expect_errors=True)
+    response = self.testapp.put_json("/api/tournaments/{}".format(id), params,
+                                     expect_errors=True)
     self.assertEqual(response.status_int, 400)
     
   def testPutTournament_invalid_num_boards(self):
     self.loginUser()
     id = self.AddBasicTournament()
     params = {'name': 'name1', 'no_pairs': 8, 'no_boards': '-1'}
-    response = self.testapp.put_json("/api/tournaments/{}".format(id), params, expect_errors=True)
+    response = self.testapp.put_json("/api/tournaments/{}".format(id), params,
+                                     expect_errors=True)
     self.assertEqual(response.status_int, 400)
     params = {'name': 'name1', 'no_pairs': 8}
-    response = self.testapp.put_json("/api/tournaments/{}".format(id), params, expect_errors=True)
+    response = self.testapp.put_json("/api/tournaments/{}".format(id), params,
+                                     expect_errors=True)
     self.assertEqual(response.status_int, 400)
     
   def testPutTournament_invalid_num_pairs(self):
     self.loginUser()
     id = self.AddBasicTournament()
     params = {'name': 'name1', 'no_pairs': -1, 'no_boards': '24'}
-    response = self.testapp.put_json("/api/tournaments/{}".format(id), params, expect_errors=True)
+    response = self.testapp.put_json("/api/tournaments/{}".format(id), params,
+                                     expect_errors=True)
     self.assertEqual(response.status_int, 400)
     params = {'name': 'name1', 'no_pairs': 1, 'no_boards': '24'}
-    response = self.testapp.put_json("/api/tournaments/{}".format(id), params, expect_errors=True)
+    response = self.testapp.put_json("/api/tournaments/{}".format(id), params,
+                                     expect_errors=True)
     self.assertEqual(response.status_int, 400)
     params = {'name': 'name1', 'no_boards': 24}
-    response = self.testapp.put_json("/api/tournaments/{}".format(id), params, expect_errors=True)
+    response = self.testapp.put_json("/api/tournaments/{}".format(id), params,
+                                     expect_errors=True)
     self.assertEqual(response.status_int, 400)
 
   def testPutTournament(self):
@@ -144,7 +155,8 @@ class AppTest(unittest.TestCase):
     id = self.AddBasicTournament()
     params = {'name': 'name2', 'no_pairs': 9, 'no_boards': 27, 
               'players' : [{ "pair_no": 1, "name": "my name" }]}
-    self.testapp.put_json("/api/tournaments/{}".format(id), params, expect_errors=True)
+    self.testapp.put_json("/api/tournaments/{}".format(id), params, 
+                          expect_errors=True)
     response = self.testapp.get("/api/tournaments/{}".format(id))
     self.assertEqual(response.status_int, 200)
     response_dict = json.loads(response.body)
@@ -153,6 +165,42 @@ class AppTest(unittest.TestCase):
     self.assertEqual(27, response_dict['no_boards'])
     self.assertEqual(1, response_dict['players'][0]["pair_no"])
     self.assertEqual("my name", response_dict['players'][0]["name"])
+
+  def testDeleteTournament_not_logged_in(self):
+    self.loginUser()
+    id = self.AddBasicTournament()
+    self.logoutUser()
+    response = self.testapp.delete("/api/tournaments/{}".format(id),
+                                   expect_errors=True)
+    self.assertEqual(response.status_int, 401)
+  
+  def testDeleteTournament_bad_id(self):
+    self.loginUser()
+    id = self.AddBasicTournament()
+    response = self.testapp.delete("/api/tournaments/{}a".format(id),
+                                   expect_errors=True)
+    self.assertEqual(response.status_int, 404)
+
+  def testDeleteTournament_not_owner(self):
+    self.loginUser()
+    id = self.AddBasicTournament()
+    self.loginUser('user2@example.com', id='234')
+    response = self.testapp.delete("/api/tournaments/{}".format(id),
+                                   expect_errors=True)
+    self.assertEqual(response.status_int, 403)
+    
+  def testDeleteTournament(self):
+    self.loginUser()
+    id = self.AddBasicTournament()
+    response = self.testapp.delete("/api/tournaments/{}".format(id))
+    self.assertEqual(response.status_int, 204)
+    response = self.testapp.get("/api/tournaments/{}".format(id),
+                                expect_errors=True)
+    self.assertEqual(response.status_int, 404)
+    response = self.testapp.get("/api/tournaments")
+    tourneys = json.loads(response.body)
+    self.assertIsNotNone(tourneys["tournaments"])
+    self.assertEqual(0, len(tourneys["tournaments"]))
 
   def loginUser(self, email='user@example.com', id='123', is_admin=False):
     self.testbed.setup_env(
