89014fff8cb4e6f06f03cf7d328442eacfa3d375
Michael Staib <mstaib.git@infinitelyeven.net>
2016-12-23 20:56:40 -0500

    Material-ize the tournament list mockup.

--- web/src/tournaments/tournament-list.js
+++ web/src/tournaments/tournament-list.js
@@ -17,13 +17,13 @@
      * @type {{id:String,name:String}[]}
      * @export
      */
-    this.tournaments = [{"id": "1234567890abcdef", "name": "example tournament"}];
+    this.tournaments = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map(function() { return {"id": "1234567890abcdef", "name": "example tournament"}; });
   }
 
   /**
    * Configures the routing provider to load the tournament list at /tournaments.
    *
-   * @param {angular.$routeProvider} $routeProvider
+   * @param {$routeProvider} $routeProvider
    * @ngInject
    */
   function mapRoute($routeProvider) {
