87c95351721aad664dff8066b8197c83621da544
Michael Staib <mstaib.git@infinitelyeven.net>
2016-12-25 23:12:29 -0500

    Fix typedef in tournament-list.

--- web/src/tournaments/tournament-list.js
+++ web/src/tournaments/tournament-list.js
@@ -28,7 +28,7 @@
    * Asynchronously loads the list of tournaments.
    *
    * @param {!angular.$q} $q
-   * @return {!angular.$q.Promise<tichu.TournamentSummary>}
+   * @return {!angular.$q.Promise<tichu.TournamentSummary[]>}
    */
   function loadTournamentList($q) {
     return $q.when([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map(function(index) {
